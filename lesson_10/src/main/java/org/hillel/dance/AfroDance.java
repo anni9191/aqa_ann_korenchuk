a55f5f26e7252db97e06dc7e7545e4d22e132b37
Ann Korenchuk <annakorenchuk9191@gmail.com>
2023-07-07 13:49:00 +0200

    Lesson10 with AfroDance was added

---
+++ lesson_10/src/main/java/org/hillel/dance/AfroDance.java
@@ -0,0 +1,21 @@
+package org.hillel.dance;
+/*
+public class AfroDance extends Dance {
+
+    public AfroDance(int tribes, String , String Monica, int, 1950) {
+
+            String moves;
+            int yearOfCreation;
+
+            public AfroDance(int choreography, String name, String creator, int yearOfCreation) {
+                super(choreography, name);
+                this.moves = moves;
+                this.yearOfCreation = yearOfCreation;
+            }
+
+            public AfroDance() {
+
+            }
+        }
+}
+*/
